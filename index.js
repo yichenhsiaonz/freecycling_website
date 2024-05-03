9b515e96a85c7b0e81ee219e35590a5b1d41e34a
Yi-chen <yhsi135@aucklanduni.ac.nz>
2024-05-03 15:27:18 +1200

    finished Assignment

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-const grow_list = document.querySelectorAll("#growflash")
-const grow_array = [...grow_list];
-const registerButton = document.getElementById("registerbutton")
+const growList = document.querySelectorAll("#growflash")
+const growArray = [...growList];
+const formBackground = document.getElementById("formbackground")
+const registerButton = document.getElementById("trigger-modal")
+const closeButton = document.getElementById("closeform")
+const registerForm = document.getElementById("form-ct")
+const blur = document.getElementById("blur")
 
 registerButton.addEventListener("click", toggleFlash);
+closeButton.addEventListener("click", toggleFlash);
+blur.addEventListener("click", toggleFlash);
 
 function toggleFlash() {
-    grow_array.forEach((element) => element.classList.toggle("registerflash"));
+    growArray.forEach((element) => element.classList.toggle("registerflash"));
+    formBackground.classList.toggle("registerflash");
+    formBackground.classList.toggle("openformbg");
+    registerButton.classList.toggle("hide");
+    registerForm.classList.toggle("fadein");
+    blur.classList.toggle("fadeinblur");
 }
